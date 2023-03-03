65e152b31d496559fbf4ced691e3a13d7cd18091
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-03-03 07:11:45 +0530

    Create BST from inorder traversal.cpp

---
+++ BST/BST from inorder traversal.cpp
@@ -0,0 +1,25 @@
+not possible to get the exact bst from which the inorder was created
+inorder is always in ascending order
+
+so inorder for trees like
+
+1.      1
+          \
+            2 
+              \
+                3
+                  \
+                    4
+                      \
+                        5
+                        
+  
+2.          3
+          /   \
+         2     4
+        /       \
+       1         5
+  
+  
+etc will be same
+                    
